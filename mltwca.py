459fadfc643d350929302e412338b12e4bdb66c8
Hector Moreno <hector2_10@hotmail.com>
2018-09-27 14:13:48 -0500

    added functions to get the attractors

--- mltwca.py
+++ mltwca.py
@@ -3,10 +3,30 @@ import random
 
 
 def main():
+	k= 9
+	if k<= 0:
+		print("Error: k must be greater than 0")
+		return -1
+
 	dataset = generateDataset(10,10)
 	dist = getDistanceMatrix(dataset)
 	print(dataset)
 	print(dist)
+	sumatory = getSumatory(dist)
+	print(sumatory)
+	if k == 1:
+		first = getFirstAttractor(sumatory)
+		return first
+	elif k == 2:
+		first = getFirstAttractor(sumatory)
+		second = getSecondAttractor(first,dist)
+		return [first,second]
+	else:
+		first = getFirstAttractor(sumatory)
+		second = getSecondAttractor(first,dist)
+		attractors = getOtherAttractors(first,second,k,dist)
+		print(attractors)
+		return attractors
 
 
 def getDistanceMatrix(dataset):
@@ -19,6 +39,66 @@ def getDistanceMatrix(dataset):
 					count += 1
 			dist[row][pattern] = count
 	return dist
+
+def getSumatory(dist):
+	sumatory = np.zeros(shape=len(dist))
+	for i in range(0,len(dist)):
+		for j in range(0,len(dist)):
+			if (i <= j):
+				sumatory[i] += dist[i][j]
+			else:
+				sumatory[i] += dist[j][i]
+	return sumatory
+
+def getFirstAttractor(sumatory):
+	return getMax(sumatory)
+
+def getSecondAttractor(firstIndex,dist):
+	maxIndex = 0
+	for j in range(0,len(dist)):
+		maxValue = 0
+		if maxIndex < firstIndex:
+			maxValue = dist[maxIndex][firstIndex]
+		else:
+			maxValue = dist[firstIndex][maxIndex]
+
+		if (j <= firstIndex):
+			if maxValue < dist[j][firstIndex]:
+				maxIndex = j
+		else:
+			if maxValue < dist[firstIndex][j]:
+				maxIndex = j
+	return maxIndex
+
+def getOtherAttractors(first,second,k,dist):
+	attractors = [first,second]
+	sumatory = np.zeros(shape=(len(dist)))
+	sumDistances(first,dist,sumatory)
+	sumDistances(second,dist,sumatory)
+	for i in range(0,k-2):
+		maxIndex = getMax(sumatory)
+		attractors.append(maxIndex)
+		sumDistances(maxIndex,dist,sumatory)
+	return attractors
+
+
+def sumDistances(index,dist,store):
+	for j in range(0,len(dist)):
+		if (j <= index):
+			store[j] += dist[j][index]
+			dist[j][index] = 0
+		else:
+			store[j] += dist[index][j]
+			dist[index][j] = 0
+
+
+
+def getMax(arr):
+	maxIndex = 0
+	for i in range(0,len(arr)):
+		if arr[maxIndex] < arr[i]:
+			maxIndex = i
+	return maxIndex
 
 def generateDataset(rows,columns):
 	dataset = np.zeros(shape=(rows,columns))
